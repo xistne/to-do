602d6abb2279f499f54fa3349f537eab795929d1
xistne <du20063@gmail.com>
2025-04-13 11:38:10 +0900

    Feat : Swagger, Log

---
+++ src/main/java/com/example/todo/config/SwaggerConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.todo.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(info());
+    }
+
+    private Info info() {
+        return new Info()
+                .title("Spring Boot Open API Test with Swagger")
+                .description("설명 부분")
+                .version("1.0.0");
+    }
+}
